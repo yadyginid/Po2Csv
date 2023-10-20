e7f8c3dbf564b2e33fdd663c4208ce84d34ac9b5
unknown <User@PC-Redin.corp.pangames.ru>
2023-10-20 17:43:11 +0300

    msgid copy from eng update po

--- CsvToPo.py
+++ CsvToPo.py
@@ -10,11 +10,35 @@ def csv_to_po(csv_file_path, output_directory, original_po_directory):
         reader = csv.DictReader(csvfile)
         languages = [field for field in reader.fieldnames if field not in ['msgid', 'msgctxt']]
 
+        # First, create the English .po file
+        if 'en' in languages:
+            original_english_po_path = os.path.join(original_po_directory, 'en', 'Game.po')
+            original_english_po = polib.pofile(original_english_po_path)
+            english_po = polib.POFile()
+            english_po.metadata = original_english_po.metadata
+
+            csv_data_en = {row['msgid']: row['en'] for row in reader}
+            csvfile.seek(0)  # Reset CSV reader position for next iteration
+
+            for entry in original_english_po:
+                if entry.msgid in csv_data_en:
+                    entry.msgstr = csv_data_en[entry.msgid]
+                english_po.append(entry)
+
+            english_po_path = os.path.join(output_directory, 'en', 'Game.po')
+            os.makedirs(os.path.dirname(english_po_path), exist_ok=True)
+            english_po.save(english_po_path)
+            print(f"English .po file saved to {english_po_path}")
+
+        # Now, create .po files for other languages
         for lang in languages:
+            if lang == 'en':
+                continue  # Skip English
+
             original_po_path = os.path.join(original_po_directory, lang, 'Game.po')
             original_po = polib.pofile(original_po_path)
             new_po = polib.POFile()
-            
+
             # Copy metadata from original .po file
             new_po.metadata = original_po.metadata
 
@@ -22,9 +46,15 @@ def csv_to_po(csv_file_path, output_directory, original_po_directory):
             csvfile.seek(0)  # Reset CSV reader position for next iteration
 
             for entry in original_po:
-                if entry.msgid in csv_data:
-                    entry.msgstr = csv_data[entry.msgid]
-                new_po.append(entry)
+                if entry.msgid in csv_data_en:
+                    new_entry = polib.POEntry(
+                        msgid=csv_data_en[entry.msgid],
+                        msgstr=csv_data.get(entry.msgid, ""),
+                        comment=entry.comment,
+                        tcomment=entry.tcomment,
+                        occurrences=entry.occurrences  # Corrected here
+                    )
+                    new_po.append(new_entry)
 
             output_po_path = os.path.join(output_directory, lang, 'Game.po')
             os.makedirs(os.path.dirname(output_po_path), exist_ok=True)
